f68a5f2c6a1aff27c6d950d6f3363bf05829c398
cathymacpherson <52889256+cathymacpherson@users.noreply.github.com>
2020-04-24 16:19:11 +0800

    Scripting Changes

--- Assets/Scripts/rotationController.cs
+++ Assets/Scripts/rotationController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using System.IO;
 using System;
 using System.Linq;
+using UnityEngine.UI;
 
 public class rotationController : MonoBehaviour
 {
@@ -16,7 +17,8 @@ public class rotationController : MonoBehaviour
     GameObject leftHandDark;
     GameObject rightHandDark;
     GameObject rightStickDark;
-    GameObject leftStickDark; 
+    GameObject leftStickDark;
+    Slider mySlider;
     List<string> stringList = new List<string>(); //stores the read in lines from the text file
      public int currentLine = 0;
      private string[] data;
@@ -25,14 +27,23 @@ public class rotationController : MonoBehaviour
        
 void Start()
 {
-    leftHandLight = GameObject.Find("/L_Hand_Light/L_Arm/L_Forearm/L_Wrist");
-    rightHandLight = GameObject.Find("/R_Hand_Light/R_Arm/R_Forearm/R_Wrist");
-    leftStickLight = GameObject.Find("/L_Stick_Light/L_Rotator");
+    
+   leftStickLight = GameObject.Find("/L_Stick_Light/L_Rotator");
     rightStickLight = GameObject.Find("/R_Stick_Light/R_Rotator");
+
+      leftHandLight = GameObject.Find("/L_Hand_Light/L_Arm/L_Forearm/L_Wrist");
+        rightHandLight = GameObject.Find("/R_Hand_Light/R_Arm/R_Forearm/R_Wrist"); 
+
+        leftStickDark = GameObject.Find("/L_Stick_Dark/L_Rotator");
+        rightStickDark = GameObject.Find("/R_Stick_Dark/R_Rotator");
+
+
     leftHandDark = GameObject.Find("/L_Hand_Dark/L_Arm/L_Forearm/L_Wrist");
-    rightHandDark = GameObject.Find("/R_Hand_Dark/R_Arm/R_Forearm/R_Wrist");
-    leftStickDark = GameObject.Find("/L_Stick_Dark/L_Rotator");
-    rightStickDark = GameObject.Find("/R_Stick_Dark/R_Rotator");
+     rightHandDark = GameObject.Find("/R_Hand_Dark/R_Arm/R_Forearm/R_Wrist");
+
+        //mySlider = GameObject.Find("Slider").GetComponent<Slider>();
+       // mySlider.gameObject.SetActive(false);
+
 
         readCSV();
 }
@@ -47,23 +58,26 @@ void Start()
 void runMovement()
     {
         currentLine++; //increases the line
-        if (currentLine >= data.Length)
-            currentLine = 0; //loop the animation if it reaches the end
+       // if (currentLine >= data.Length)
+        //    mySlider.gameObject.SetActive(true); //loop the animation if it reaches the end
         row = data[currentLine].Split(','); //loops through data and splits data into array by comma
         
         for (int i = 0; i < data.Length - 1; i++)
         {
             rightStickLight.transform.eulerAngles = new Vector3(rightStickLight.transform.eulerAngles.x, rightStickLight.transform.eulerAngles.y, float.Parse(row[0])*20);
             leftStickLight.transform.eulerAngles = new Vector3(leftStickLight.transform.eulerAngles.x, leftStickLight.transform.eulerAngles.y, 0 - float.Parse(row[1])*20); 
-            rightHandLight.transform.eulerAngles = new Vector3(rightHandLight.transform.eulerAngles.x, rightHandLight.transform.eulerAngles.y, 90 + float.Parse(row[0])*20);
-            leftHandLight.transform.eulerAngles = new Vector3(leftHandLight.transform.eulerAngles.x, leftHandLight.transform.eulerAngles.y, 270 + float.Parse(row[1])*20);
 
-            rightStickDark.transform.eulerAngles = new Vector3(rightStickDark.transform.eulerAngles.x, rightStickDark.transform.eulerAngles.y, float.Parse(row[0]) * 20);
-            leftStickDark.transform.eulerAngles = new Vector3(leftStickDark.transform.eulerAngles.x, leftStickDark.transform.eulerAngles.y, 0 - float.Parse(row[1]) * 20);
-            rightHandDark.transform.eulerAngles = new Vector3(rightHandDark.transform.eulerAngles.x, rightHandDark.transform.eulerAngles.y, 90 + float.Parse(row[0]) * 20);
-            leftHandDark.transform.eulerAngles = new Vector3(leftHandDark.transform.eulerAngles.x, leftHandDark.transform.eulerAngles.y, 270 + float.Parse(row[1]) * 20); 
+           // rightHandLight.transform.eulerAngles = new Vector3(rightHandLight.transform.eulerAngles.x, rightHandLight.transform.eulerAngles.y, 90 + float.Parse(row[0])*20);
+          //  leftHandLight.transform.eulerAngles = new Vector3(leftHandLight.transform.eulerAngles.x, leftHandLight.transform.eulerAngles.y, 270 + float.Parse(row[1])*20);
+
+           // rightStickDark.transform.eulerAngles = new Vector3(rightStickDark.transform.eulerAngles.x, rightStickDark.transform.eulerAngles.y, float.Parse(row[0]) * 20);
+          //  leftStickDark.transform.eulerAngles = new Vector3(leftStickDark.transform.eulerAngles.x, leftStickDark.transform.eulerAngles.y, 0 - float.Parse(row[1]) * 20);
+
+          //  rightHandDark.transform.eulerAngles = new Vector3(rightHandDark.transform.eulerAngles.x, rightHandDark.transform.eulerAngles.y, 90 + float.Parse(row[0]) * 20);
+          //  leftHandDark.transform.eulerAngles = new Vector3(leftHandDark.transform.eulerAngles.x, leftHandDark.transform.eulerAngles.y, 270 + float.Parse(row[1]) * 20); 
         }
     }
+
 
 
 void Update()
